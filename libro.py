da870e06e263fddd118b744fd8a0a0832146bbfd
Filippo Di Michele <dimichelefilippo06@gmail.com>
2026-01-29 11:28:16 +0100

    Refactor Libro class constructor and methods for better functionality

--- libro.py
+++ libro.py
@@ -1,3 +1,18 @@
 class Libro:
-    def __init__(self, libro):
-        self
+    def __init__(self,titolo, isbn, autore):
+        self.titolo = titolo
+        self.isbn = isbn
+        self.disponibile = True
+        
+        
+    def _isdisponibile(self, disponibile):
+        return self._isdisponibile
+    
+    def prestito(self):
+        self.disponibile = False
+        
+    def restituisci(self):
+        self.disponibile = True
+        
+    def __str__(self):
+        stato = "Disponibile"
