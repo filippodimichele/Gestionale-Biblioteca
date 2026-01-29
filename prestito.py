6b8656013ad54203171d50bae57a56b1f2734625
Filippo Di Michele <dimichelefilippo06@gmail.com>
2026-01-29 11:52:14 +0100

    Refactor Prestito class constructor to accept libro, utente, and data_prestito parameters

--- prestito.py
+++ prestito.py
@@ -1,3 +1,8 @@
 class Prestito:
-    def __init__(self, prestito):
-        self
+    def __init__(self, libro, utente, data_prestito):
+        self.libro = libro
+        self.utente = utente 
+        self.data_prestito = data_prestito
+        self.data_restituzione = None
+        
+        
