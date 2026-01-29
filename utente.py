f709d90022e689958b7e4d98c6fb565f8012de83
Filippo Di Michele <dimichelefilippo06@gmail.com>
2026-01-29 11:59:32 +0100

    Refactor Utente class constructor to accept nome and numero_tessera parameters

--- utente.py
+++ utente.py
@@ -1,3 +1,5 @@
 class Utente:
-    def __init__(self, utente):
-        self 
+    def __init__(self, nome, numero_tessera):
+        self.nome = nome
+        self.numero_tessera = numero_tessera
+        self.prestiti = []
