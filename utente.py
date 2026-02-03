4074691d0becd9dd004d5e2dc42414339eabedd7
Filippo <la-tua-email@esempio.com>
2026-02-03 16:59:00 +0100

    implementazione nuova versione del file

--- utente.py
+++ utente.py
@@ -1,15 +1,26 @@
 class Utente:
+
+    MAX_PRESTITI = 3
+
     def __init__(self, nome, numero_tessera):
         self.nome = nome
         self.numero_tessera = numero_tessera
         self.prestiti = []
-        
-    def aggiungi_prestiti(self, prestiti):
-        self.prestiti = prestiti
-        
-    def storico_prestiti(self):
-        return self.prestiti
-    
-    #max 3 prestiti
-    def __str__(self):
-        return f"{self.nome} (Tessera {self.numero_tessera})"
+
+    def __repr__(self):
+        return f"Utente(nome='{self.nome}', tessera='{self.numero_tessera}', prestiti_attivi={len(self.prestiti)})"
+
+    def __eq__(self, other):
+        if not isinstance(other, Utente):
+            return False
+        return self.numero_tessera == other.numero_tessera
+
+    def puo_prendere_in_prestito(self):
+        return len(self.prestiti) < self.MAX_PRESTITI
+
+    def aggiungi_prestito(self, prestito):
+        self.prestiti.append(prestito)
+
+    def rimuovi_prestito(self, prestito):
+        if prestito in self.prestiti:
+            self.prestiti.remove(prestito)
